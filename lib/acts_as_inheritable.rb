08daabc105455b9a3a5b31f8f7087354b4614ff5
Rob Momary <rob@deepspacerobots.com>
2018-09-01 06:53:29 -0500

    update to include recursive methods going UP the chain

--- lib/acts_as_inheritable.rb
+++ lib/acts_as_inheritable.rb
@@ -31,36 +31,6 @@ module ActsAsInheritable
         end
       end
 
-      def apply_relations_to_parent(model_parent = send(:parent), current = self)
-        if model_parent && current.class.method_defined?(:inheritable_configuration) && current.class.inheritable_configuration[:associations]
-          current.class.inheritable_configuration[:associations].each do |relation|
-            child_relation = send(relation)
-            relation_instances = child_relation.respond_to?(:each) ? child_relation : [child_relation].compact
-            relation_instances.each do |relation_instance|
-              apply_instance_to_parent(current, model_parent, relation, relation_instance)
-            end
-          end
-        end
-      end
-
-      def apply_instance_to_parent(current, model_parent, relation, relation_instance)
-        new_relation = relation_instance.dup
-        belongs_to_associations_names = current.class.reflect_on_all_associations(:belongs_to).collect(&:name)
-        saved =
-          # Is a `belongs_to` association
-          if belongs_to_associations_names.include?(relation.to_sym)
-            # You can define your own 'dup' method with a `duplicate!` signature
-            new_relation = relation_instance.duplicate! if relation_instance.respond_to?(:duplicate!)
-            model_parent.send("#{relation}=", new_relation)
-            model_parent.save
-          # else
-          #   # Is a `has_one | has_many` association
-          #   parent_name = verify_parent_name(new_relation, model_parent)
-          #   new_relation.send("#{parent_name}=", current)
-          #   new_relation.save
-          end
-          apply_relations_to_parent(relation_instance, new_relation) if saved
-      end
 
       def inherit_instance(current, model_parent, relation, relation_instance)
         new_relation = relation_instance.dup
@@ -120,6 +90,68 @@ module ActsAsInheritable
           end
         end
       end
+
+       # This is an inheritable recursive method that iterates over all of the
+      # relations defined on `INHERITABLE_ASSOCIATIONS`. For each instance on
+      # each relation it re-creates it - this method goes up the chain from child to parent.
+      #
+      def apply_relations_to_parent(model_parent = send(:parent), current = self)
+        if model_parent && current.class.method_defined?(:inheritable_configuration) && current.class.inheritable_configuration[:associations]
+          current.class.inheritable_configuration[:associations].each do |relation|
+            child_relation = send(relation)
+            relation_instances = child_relation.respond_to?(:each) ? child_relation : [child_relation].compact
+            relation_instances.each do |relation_instance|
+              apply_instance_to_parent(current, model_parent, relation, relation_instance)
+            end
+          end
+        end
+      end
+
+      def apply_instance_to_parent(current, model_parent, relation, relation_instance)
+        new_relation = relation_instance.dup
+        belongs_to_associations_names = current.class.reflect_on_all_associations(:belongs_to).collect(&:name)
+
+          # Is a `belongs_to` association
+          if belongs_to_associations_names.include?(relation.to_sym)
+
+            # You can define your own 'dup' method with a `duplicate!` signature
+            new_relation = relation_instance.duplicate! if relation_instance.respond_to?(:duplicate!)
+            model_parent.send("#{relation}=", new_relation)
+            saved = model_parent.save
+          else
+
+            # Is a `has_one | has_many` association
+            child_name = verify_child_name(new_relation, current)
+            new_relation.send("#{child_name}=", model_parent)
+            saved = new_relation.save
+          end
+          apply_relations_to_parent(relation_instance, new_relation) if saved
+      end
+
+      def verify_child_name(new_relation, current)
+
+        child_name = current.class.to_s.downcase
+        return child_name if new_relation.respond_to?(child_name)
+        many_and_one_associations = current.class.reflect_on_all_associations.select { |a| a.macro != :belongs_to }
+        many_and_one_associations.each do |association|
+          next unless association.klass.to_s.downcase == new_relation.class.to_s.downcase && association.options.key?(:as)
+          as = association.options[:as].to_s
+          if new_relation.respond_to?(as) && !new_relation.respond_to?(child_name)
+            child_name = as
+            break
+          end
+        end
+        # Relations has a different name
+        unless new_relation.respond_to?(child_name)
+          new_relation.class.reflections.each_key do |reflection|
+            next unless new_relation.class.reflections[reflection].class_name == current.class.name
+            child_name = reflection
+            break
+          end
+        end
+        child_name
+      end
+
 
       def apply_attributes_to_parent(force = false, not_force_for = [], method_to_update = nil)
         available_methods = ['update_attributes', 'update_columns']
--- spec/acts_as_inheritable_spec.rb
+++ spec/acts_as_inheritable_spec.rb
@@ -193,17 +193,18 @@ RSpec.describe "ActiveRecord::Base model with #acts_as_inheritable" do
 
 
   describe '#apply_relations_to_parent' do
-    # describe '`has_one` associations' do
-    #   let(:person_parent) { create(:person, :with_pet) }
-    #   let!(:person){ create(:person, parent: person_parent) }
-#
-    #   it 're-creates the parent pet from the child pet' do
-    #     expect {
-    #       person.apply_relations_to_parent
-    #     }.to change(Pet, :count).by(1)
-    #     expect(person.pet.id).to_not eq person_parent.pet.id
-    #   end
-    # end
+    describe '`has_one` associations' do
+      let(:person_parent){ create(:person) }
+      let!(:person) { create(:person, :with_pet, parent: person_parent) }
+
+      it 're-creates the parent pet from the child pet' do
+       expect {
+         person.apply_relations_to_parent
+       }.to change(Pet, :count).by(1)
+       expect(person_parent.pet.id).to_not eq person.pet.id
+      end
+    end
+
     describe '`belongs_to` associations' do
       let(:person_parent) { create(:person) }
       let!(:person){ create(:person, :with_clan, parent: person_parent) }
@@ -217,34 +218,34 @@ RSpec.describe "ActiveRecord::Base model with #acts_as_inheritable" do
     end
     describe '`has_many` associations' do
 
-      let(:person_parent) { create(:person, :with_shoes, number_of_shoes: 2) }
-      let!(:person){ create(:person, parent: person_parent) }
+      let(:person_parent) { create(:person) }
+      let!(:person){ create(:person, :with_shoes, number_of_shoes: 2, parent: person_parent) }
 
       it 're-creates all the parent shoes from the child shoes' do
         expect {
-          person.inherit_relations
-        }.to change(Shoe, :count).by(person_parent.shoes.count)
+          person.apply_relations_to_parent
+        }.to change(Shoe, :count).by(person.shoes.count)
       end
 
       context 'when association is polymorphic' do
-        let(:person_parent) { create(:person, :with_pictures, number_of_pictures: 2) }
-        let!(:person){ create(:person, parent: person_parent) }
+        let(:person_parent) { create(:person) }
+        let!(:person){ create(:person, :with_pictures, number_of_pictures: 2, parent: person_parent) }
 
         it 're-creates all the parent pictures from the child pictures' do
           expect {
-            person.inherit_relations
-          }.to change(Picture, :count).by(person_parent.pictures.count)
+            person.apply_relations_to_parent
+          }.to change(Picture, :count).by(person.pictures.count)
         end
       end
 
       context 'when association uses a different name' do
-        let(:person_parent) { create(:person, :with_toys, number_of_toys: 2) }
-        let!(:person){ create(:person, parent: person_parent) }
+        let(:person_parent) { create(:person) }
+        let!(:person){ create(:person, :with_toys, number_of_toys: 2, parent: person_parent) }
 
         it 're-creates all the parent toys from the child toys' do
           expect {
-            person.inherit_relations
-          }.to change(Toy, :count).by(person_parent.toys.count)
+            person.apply_relations_to_parent
+          }.to change(Toy, :count).by(person.toys.count)
         end
       end
     end
